29e4c829ab9c7e8391a47bad18f7820983ff34e8
aditya9855 <aditya9855@gmail.com>
2022-04-14 17:07:48 +0530

    added datatable jquery master bootstrap

--- README.md
+++
@@ -1,1 +0,0 @@
-This is Simple Project which consists crud operations using php and mysql as database.
---
+++ project_return.php
@@ -0,0 +1,27 @@
+<?php
+ 
+include ("db.php");
+ 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 
+    $stmt = $conn->prepare("select id,name,course,fees,status from records where id = ?");
+    $stmt->bind_param("s",$project_id);
+    $stmt->bind_result($id, $name, $course, $fees, $status);
+ 
+ 
+    $project_id = $_POST['project_id'];
+ 
+    if ($stmt->execute()) {
+        while ($stmt->fetch()) {
+            $output = array('id' => $id, 'name' => $name, 'course' => $course, 'fees' => $fees, 'status' => $status);
+        }
+        echo json_encode($output);
+    }
+ 
+ 
+    $stmt->close();
+ 
+}
+ 
+ 
+?>
